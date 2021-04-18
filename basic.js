87615ebe89ea15c5b6e31927bff4067763d8d931
tayloramitverma <tayloramitverma@gmail.com>
2021-04-18 10:00:47 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,54 @@
+console.log(true && 'abc')
+
+console.log(false && 'abc')
+
+console.log(true || 'abc')
+
+console.log(false || 'abc')
+
+function a(start){
+    console.log('start')
+    start++;
+    return function(){
+        console.log('in-side');
+        start++;
+        return start;
+    }
+}
+
+var test = a(3);
+console.log(test());
+console.log(test());
+console.log(test());
+console.log(test());
+
+for(let i = 0 ; i < 5 ; i++){
+    setTimeout(()=>{
+        console.log(i)
+    },100)
+}
+for(var i = 0 ; i < 5 ; i++){
+    setTimeout(()=>{
+        console.log(i)
+    })
+}
+
+
+var result = [];
+for (var i=0; i < 5; i++) {
+    (function () {
+        var i2 = i; // copy current i
+        result.push(function () { return i2 });
+    }());
+}
+
+var result1 = [];
+for (let j=0; j < 5; j++) {
+    (function () {
+        let i2 = j; // copy current i
+        result1.push(function () { return i2 });
+    }());
+}
+
+console.log(result[1]())
+console.log(result1[4]())
---
+++ promise.js
@@ -0,0 +1,74 @@
+let giphyAPI = 'https://api.giphy.com/v1/gifs/search?api_key=GhlfmlP64keupcoUbhsOYT78r10OSM6H&q=';
+
+let randWords = 'https://random-word-api.herokuapp.com/word?key=06DQTLAP&number=1';
+
+function setup() {
+  noCanvas();
+  
+ // loadJSON(giphyAPI, gotData);
+  
+  let promises = [];
+  for(let x=0; x < 10; x++){
+	promises.push(wordGIF());
+  }
+  
+  Promise.all(promises)
+  .then(results => {
+    
+    for(let x=0; x < results.length; x++){
+
+        createP(capitalize(results[x].text));
+        createImg(results[x].img);
+    }
+  
+  })
+  .catch((err)=> console.log(err));
+  
+  
+  
+  /*
+  wordGIF()
+  .then(results =>{
+    createImg(results.img);
+  })
+  .catch((err)=> console.log(err));
+  
+  wordGIF()
+  .then(results =>{
+    createImg(results.img);
+  })
+  .catch((err)=> console.log(err));
+  
+  wordGIF()
+  .then(results =>{
+    createImg(results.img);
+  })
+  .catch((err)=> console.log(err));
+  */
+  
+  
+}
+
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+async function wordGIF(){
+
+  let response1 = await fetch(randWords);
+  let json1 = await response1.json();
+
+  let response2 = await fetch(giphyAPI+json1[0]);
+  let json2 = await response2.json();
+
+  let img_url = json2.data[0].images['fixed_height_small'].url;
+
+  return {
+    img : img_url,
+    text: json1[0]
+  }
+  
+}
+
+
