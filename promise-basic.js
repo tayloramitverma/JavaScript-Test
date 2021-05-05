c2b7c8dfd5bc2fd583fbb5bdeb0e8d04087dd5a8
tayloramitverma <tayloramitverma@gmail.com>
2021-05-05 17:11:48 +0530

    updated code

---
+++ promise-basic.js
@@ -0,0 +1,46 @@
+// Promise: Best video on promises
+// -resolve
+// -reject
+// -pending
+
+function func1() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            const error = true;
+            if (!error) {
+                console.log('Function: Your promise has been resolved')
+                resolve();
+            }
+            else {
+                console.log('Function: Your promise has not been resolved')
+                reject('Sorry not fulfilled');
+            }
+        }, 2000);
+    })
+}
+
+//async & await
+
+func1().then(function(){
+    console.log("Harry: Thanks for resolving")
+}).catch(function(error){
+    console.log("Harry: Very bad bro. Reason: " + error)
+})
+
+async function harry(){
+    console.log('Inside harry function');
+    const response = await fetch('https://api.github.com/users');
+    console.log('before response');
+    const users = await response.json();
+    console.log('users resolved')
+    return users;
+
+    // return "harry";
+}
+
+console.log("Before calling harry")
+let a = harry();
+console.log("After calling harry")
+console.log(a);
+a.then(data => console.log(data))
+console.log("Last line of this js file")
