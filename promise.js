ab340088c9a1a56a82ec998b1390938d89d6dfb6
tayloramitverma <tayloramitverma@gmail.com>
2021-05-05 16:37:31 +0530

    updated

--- promise.js
+++ promise.js
@@ -1,31 +1,28 @@
-let giphyAPI = 'https://api.giphy.com/v1/gifs/search?api_key=GhlfmlP64keupcoUbhsOYT78r10OSM6H&q=';
+let giphyAPI =
+  "https://api.giphy.com/v1/gifs/search?api_key=GhlfmlP64keupcoUbhsOYT78r10OSM6H&q=";
 
-let randWords = 'https://random-word-api.herokuapp.com/word?key=06DQTLAP&number=1';
+let randWords =
+  "https://random-word-api.herokuapp.com/word?key=06DQTLAP&number=1";
 
 function setup() {
   noCanvas();
-  
- // loadJSON(giphyAPI, gotData);
-  
-  let promises = [];
-  for(let x=0; x < 10; x++){
-	promises.push(wordGIF());
-  }
-  
-  Promise.all(promises)
-  .then(results => {
-    
-    for(let x=0; x < results.length; x++){
 
+  // loadJSON(giphyAPI, gotData);
+
+  let promises = [];
+  for (let x = 0; x < 10; x++) {
+    promises.push(wordGIF());
+  }
+
+  Promise.all(promises)
+    .then((results) => {
+      for (let x = 0; x < results.length; x++) {
         createP(capitalize(results[x].text));
         createImg(results[x].img);
-    }
-  
-  })
-  .catch((err)=> console.log(err));
-  
-  
-  
+      }
+    })
+    .catch((err) => console.log(err));
+
   /*
   wordGIF()
   .then(results =>{
@@ -45,30 +42,24 @@ function setup() {
   })
   .catch((err)=> console.log(err));
   */
-  
-  
 }
 
 const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
 
-async function wordGIF(){
-
+async function wordGIF() {
   let response1 = await fetch(randWords);
   let json1 = await response1.json();
 
-  let response2 = await fetch(giphyAPI+json1[0]);
+  let response2 = await fetch(giphyAPI + json1[0]);
   let json2 = await response2.json();
 
-  let img_url = json2.data[0].images['fixed_height_small'].url;
+  let img_url = json2.data[0].images["fixed_height_small"].url;
 
   return {
-    img : img_url,
-    text: json1[0]
-  }
-  
+    img: img_url,
+    text: json1[0],
+  };
 }
-
-
--- test.js
+++ test.js
@@ -1,68 +1,60 @@
-function foo(){
-var x = 100;
-console.log(x);
-(function too(){
-console.log(x);
-}());
-voo();
-console.log(x);
+function foo() {
+  var x = 100;
+  console.log(x);
+  (function too() {
+    console.log(x);
+  })();
+  voo();
+  console.log(x);
 }
 
-function voo(){
-    console.log(x);
+function voo() {
+  console.log(x);
 }
 
 var x = 200;
 console.log(x); //200
-foo();  // 100 100 200 100
-console.log(x);  200
+foo(); // 100 100 200 100
+console.log(x);
+200;
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+let myEven = arr.filter((num) => {
+  if (num % 2 === 0) {
+    return num;
+  }
+});
 
-    
-const arr = [1,2,3,4,5,6,7,8,9,10]
+fetch("hdfgfd")
+  .then((res) => {
+    res.json();
+  })
+  .then((resData) => {
+    //here can cll another one
+    fetch("sdfsdf").then((res) => {
+      return res;
+    });
+    return resData;
+  })
+  .catch((err) => console.log(err));
 
-
-let myEven = arr.filter((num)=>{
-    if(num%2 === 0){
-        return num
-    }
-})
-
- fetch('hdfgfd')
- .then(res=>{
-    res.json()
- })
- .then(resData=>
-     {
-         //here can cll another one
-        fetch('sdfsdf').then(res=>{
-            return res;
-        })
-        return resData
-     }
- )
- .catch(err=>console.log(err))
- 
-
- function foo(){
-    console.log("First")
-    setTimeout(function(){
+function foo() {
+  console.log("First");
+  setTimeout(function () {
     console.log("Second");
-    }, 0);
-    console.log("Third")
+  }, 0);
+  console.log("Third");
 }
 
-foo()
-
-
+foo();
 
 let emp = {
-    name: "Aditya",
-    salary: {
-        fixed: 10000,
-        variable : 1000
-    }
-}
+  name: "Aditya",
+  salary: {
+    fixed: 10000,
+    variable: 1000,
+  },
+};
 
-let newEmp = {...emp}
+let newEmp = { ...emp };
